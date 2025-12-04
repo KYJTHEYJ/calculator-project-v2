813e10ce9727d1859cc2af8881327217b8bdbb32
KYJ <nananan1213@gmail.com>
2025-12-04 15:43:16 +0900

    feat: step1의 반복문을 사용하되, exit 문자열 입력전 무한 계산 기능 구현 fix: 빈 칸 등이 추가하여 입력하거나 형식에 맞지 않게 입력시 런타임 에러 발생하지 않도록 조치 (try-catch 등 추가) refactor : 결과값의 정확도를 높이기 위해 double 형식을 입력받고 처리하도록 코드 수정

--- src/com/nbcamp/calculatorV2/step1/Calculator.java
+++ src/com/nbcamp/calculatorV2/step1/Calculator.java
@@ -2,31 +2,26 @@ package com.nbcamp.calculatorV2.step1;
 
 import java.util.Scanner;
 
-/*
- * TODO List
- *  Step1 require
- *  1. 타 클래스 없이 기본적 연산 수행
- *  2. 양의 정수를 입력 받기
- *  3. 사칙연산 기호 입력받기
- *  4. 위에서 입력받은 양의 정수 2개, 사칙연산 기호를 사용하여 결과값 출력하기
- *   4-1. 입력 받은 연산자의 구분은 제어문으로 처리할 것
- *   4-2. 연산 오류가 발생할 경우 해당 오류에 대한 내용을 정제 ex) 0으로 나누기 등
- *  5. 반복문을 사용하되, 반복의 종료를 알려주는 "exit" 입력 전 까진 무한 계산 가능 하도록
- */
-
 public class Calculator {
     // 계산할 숫자를 받아오기
     // 계산 받을 숫자의 갯수 지정, 0 포함한 양의 정수인지 입력 검사 수행, 잘못되면 단계별 다시 입력 받도록 조치
-    private static int[] inputNumbersWithCheckNegative(Scanner sc) {
+    private static double[] inputNumbersWithCheckNegative(Scanner sc) {
         // 계산 받을 숫자의 갯수 지정, 배열의 크기로 제어
-        int[] inputNumberArr = new int[2];
+        double[] inputNumberArr = new double[2];
 
         // 숫자 입력 받기 (0 이상일 경우만 받도록)
         for (int index = 0; index < inputNumberArr.length; index++) {
             while (true) {
+                int inputNumber;
+
                 System.out.printf("%d 번째 숫자를 입력하세요 (0 포함 양의 정수) : ", index + 1);
-                int inputNumber = sc.nextInt();
-                sc.nextLine();
+
+                try {
+                    inputNumber = Integer.parseInt(sc.nextLine());
+                } catch (Exception e) {
+                    System.out.println("잘못된 입력을 하셨어요!");
+                    continue;
+                }
 
                 if (inputNumber >= 0) {
                     inputNumberArr[index] = inputNumber;
@@ -63,44 +58,60 @@ public class Calculator {
     // 입력 받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산 진행하기
     // 0으로 나눠 질 경우 불가능하다는 결과 출력 후
     // 두번째 인수를 다시 받아 정상적으로 결과를 출력하도록 수정
-    private static void processingCal(int[] numberArr, char operator, Scanner sc) {
+    private static void processingCal(double[] numberArr, char operator, Scanner sc) {
         switch (operator) {
-            case '+' -> System.out.printf("결과 : %d\n", numberArr[0] + numberArr[1]);
-            case '-' -> System.out.printf("결과 : %d\n", numberArr[0] - numberArr[1]);
-            case '*' -> System.out.printf("결과 : %d\n", numberArr[0] * numberArr[1]);
+            case '+' -> System.out.printf("결과 : %.0f\n", numberArr[0] + numberArr[1]);
+            case '-' -> System.out.printf("결과 : %.0f\n", numberArr[0] - numberArr[1]);
+            case '*' -> System.out.printf("결과 : %.0f\n", numberArr[0] * numberArr[1]);
             case '/' -> {
                 if (numberArr[1] == 0) {
                     System.out.println("0 으로 나누는 건 불가능해요!");
 
                     while (true) {
+                        int inputNumber;
+
                         System.out.printf("%d 번째로 입력한 숫자를 바꿔주세요 (0 초과 양의 정수) : ", numberArr.length);
 
-                        int inputNumber = sc.nextInt();
+                        try {
+                            inputNumber = Integer.parseInt(sc.nextLine());
+                        } catch (Exception e) {
+                            System.out.println("잘못된 입력을 하셨어요!");
+                            continue;
+                        }
+
                         if (inputNumber > 0) {
-                            System.out.printf("결과 : %d\n", numberArr[0] / inputNumber);
-                            sc.nextLine();
+                            System.out.printf("결과 : %.1f\n", numberArr[0] / inputNumber);
                             break;
                         } else {
                             System.out.println("0 초과 양의 정수를 입력해주세요!");
                         }
                     }
                 } else {
-                    System.out.printf("결과 : %d\n", numberArr[0] / numberArr[1]);
+                    System.out.printf("결과 : %.2f\n", numberArr[0] / numberArr[1]);
                 }
             }
         }
     }
 
+    // 결과 이후 "exit" 입력시 프로그램 종료, 아무 키워드 입력시엔 계속
+    private static boolean exitCalculator(Scanner sc) {
+        String command;
+        System.out.print("계속 계산하시겠어요? ( 아무 키워드 입력시 계속 / exit 입력시 종료 ) : ");
+        command = sc.nextLine();
+        return command.equals("exit");
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] resultNumberArr = inputNumbersWithCheckNegative(sc);
-        System.out.println("resultNumberArr1: " + resultNumberArr[0]);
-        System.out.println("resultNumberArr2: " + resultNumberArr[1]);
 
-        char resultOperator = inputOperator(sc);
-        System.out.println("resultOperator: " + resultOperator);
+        while (true) {
+            double[] resultNumberArr = inputNumbersWithCheckNegative(sc);
+            char resultOperator = inputOperator(sc);
+            processingCal(resultNumberArr, resultOperator, sc);
 
-        processingCal(resultNumberArr, resultOperator, sc);
+            if (exitCalculator(sc)) {
+                break;
+            }
+        }
     }
 }
